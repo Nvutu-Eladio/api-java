073cd37402722f328b1725e507fd087ec2d790e3
Nvutu-Eladio <Eladio2019@hotmail.com>
2024-05-25 22:20:49 -0300

    Dockerfile deploy

---
+++ src/main/java/br/com/nveladio/todoList/user/UserModel.java
@@ -0,0 +1,56 @@
+package br.com.nveladio.todoList.user;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users") //no banco o nome da tabela será tb_users
+public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    // @Column(name = "usuario") // Para caso eu precissasse mudar o nome da tabela no banco de dados
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    // public void setUsername(String username) {
+    //     this.username = username;
+    // }
+
+    // public String getUsername() {
+    //     return username;
+    // }
+
+    // public void setName(String name) {
+    //     this.name = name;
+    // }
+
+    // public String getName() {
+    //     return name;
+    // }
+
+    // public void setPassword(String password) {
+    //     this.password = password;
+    // }
+
+    // public String getPassword() {
+    //     return password;
+    // }
+
+    // getters and setters
+}
